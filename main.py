9c1a687fe643d497486c7a8282f04627e7d2dbc2
GEORGE-GICHURU <george.gichuru@student.moringaschool.com>
2025-01-09 00:01:48 +0300

    pick random

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import random
 
 last_picked = None
+
+def pick_random():
+    global last_picked
+    
+    numbers = [1, 2, 3, 4, 5]
+
+    if numbers is not None:
+        numbers.remove(last_picked)
+
+    picked = random.choice(numbers)
+    last_picked = picked
+    return picked
+
+    for _ in range(5):
+        print (pick_random())
