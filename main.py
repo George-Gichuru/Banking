061c4fca5f561232baeb7e250f4f946aa70c0981
GEORGE-GICHURU <george.gichuru@student.moringaschool.com>
2025-01-11 00:11:07 +0300

    is_running

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Banking Program
 
 def Show_Balance():
-    pass
+    print(f"Your balance is {balance:.2f}")
 
 def Deposit():
     pass
@@ -10,6 +10,26 @@ def Deposit():
 def Withdraw():
     pass
 
-
+balance = 0
 is_running = True 
-# if false exit the program
+
+while is_running:
+    print("Banking Program")
+    print("1. Show Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        Show_Balance()
+    elif choice == 2:
+        Deposit()
+    elif choice == 3:
+        Withdraw()
+    elif choice == 4:
+        is_running = False
+    else:
+        print("Invalid choice")
+
+print("Thank you have a nice day!")
