341f7178d4fb9373cc8a032c6b6df75f6d8f558e
GEORGE-GICHURU <george.gichuru@student.moringaschool.com>
2025-01-11 00:42:34 +0300

    is_running

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ def Show_Balance():
     print(f"Your balance is {balance:.2f}")
 
 def Deposit():
-    pass
+    amount = float(input("Enter the amount to deposit: "))
+
+    if amount < 0:
+        print("Invalid amount")
+    else:
+        return amount
 
 def Withdraw():
     pass
@@ -19,12 +24,13 @@ while is_running:
     print("2. Deposit")
     print("3. Withdraw")
     print("4. Exit")
-    choice = int(input("Enter your choice: "))
+
+    choice = input("Enter your choice: ")
 
     if choice == 1:
         Show_Balance()
     elif choice == 2:
-        Deposit()
+        balance += Deposit()
     elif choice == 3:
         Withdraw()
     elif choice == 4:
